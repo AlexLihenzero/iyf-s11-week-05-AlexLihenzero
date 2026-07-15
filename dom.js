a150f0b13f136c97d62bd35ec9fc251a8b9216a2
unknown <alexlyhenzero@gmail.com>
2026-07-15 12:23:52 +0300

    Setup Week 5 DOM practice

---
+++ dom.js
@@ -0,0 +1,11 @@
+// ===================================
+// Lesson 9 Task 9.1
+// DOM Selection Practice
+// ===================================
+
+
+// Select header by ID
+
+const header = document.getElementById("main-header");
+
+console.log("Header:", header);
